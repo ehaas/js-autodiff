f540a882f96d740af22d2e304c64848eb624e44c
Evan Haas <evan@fictivekin.com>
2011-12-23 17:00:53 -0500

    add example for assignment operators, and a more complex numerical function (approximation of gamma function)

--- examples.js
+++ examples.js
@@ -5,6 +5,11 @@ function test(fn, at, derivative) {
 	assert.equal(diff(fn, at), derivative.call(null, at));
 }
 
+function err(fn, at, derivative) {
+	return Math.abs(diff(fn, at) - derivative.call(null, at));
+}
+
+
 function xsquared(x) {
 	return x * x;
 }
@@ -29,10 +34,43 @@ function polynomial(x) {
 	return x*x*x*x + (2*x+2) * (2*x+2);
 }
 
+function test_assignment(x) {
+	var result = 1;
+	result *= x*x+x;
+	result += x;
+	result -= 1;
+	return result;
+}
+
+//approximation of the gamma function
+function gamma(x) {
+	var w = 1/x;
+	return Math.exp((x - 0.5)*Math.log(x) - x) * Math.sqrt(2*Math.PI) * (1+w*(1/12 + w*(1/288 - w*(139/51840 - w*(571/2488320)))));
+}
+
+
+function fact(n) {
+	if (n === 0 || n === 1) return 1;
+	return n * fact(n-1);
+}
+
 test(xsquared, 3, function(x) { return 2*x; });
 test(xcubed, 4, function(x) { return 3*x*x; });
 test(e_xsquared, 2, function(x) { return 2*x*Math.exp(x*x); });
 test(e_sin_x2, Math.PI/2, function(x) { return 2*x*Math.exp(Math.sin(x*x))*Math.cos(x*x); });
 test(x_over_ln_x, 5, function(x) { return (Math.log(x) - 1) / (Math.log(x) * Math.log(x)); });
 test(polynomial, 7, function(x) { return 4*x*x*x + 8*x + 8; });
+test(test_assignment, 10, function(x) { return 2 * x + 2;});
+test(gamma, 5.3, function() { return 59.798083889488986 }); //verified with wolfram alpha
+
+//Note: only for positive integer x
+assert.ok(0.00001 > err(gamma, 4, function(x) {
+	var c = 0.5772156649015329; //Euler–Mascheroni constant
+	var m = x-1, sum = 0;
+	for (var k = 1; k <= m; k++) {
+		sum += 1/k;
+	}
+	return fact(m) * (-c + sum);
+}));
+
 
